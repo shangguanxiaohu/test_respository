a9291b87944ef07cf433e7c27a9bd2f44811414e
shangguanxiaohu <1247768069@qq.com>
2015-03-21 23:20:21 +0800

    third time commit

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,18 @@
 
 using namespace std;
 
+int  max_val(int x,int y)
+{
+	return x>y?x:y
+}
+
 int  main (int argc, char** argv)
 {
 	string str;
 	cin>>str;
-	
+
+	int  x = 5, y = 6;
+	cout<<max_val(x,y);
 	cout<<str;
 	return 0;
 }
